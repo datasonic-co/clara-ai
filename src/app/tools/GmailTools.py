f4de143ddf6bc075a5716357b18925c3e700870a
Fakhrovski <fakher.hannafi@gmail.com>
2024-12-16 02:13:03 +0100

    hotfix: fixing

--- src/app/tools/GmailTools.py
+++ src/app/tools/GmailTools.py
@@ -90,7 +90,7 @@ async def gmail_send_mail(subject, cc, body, attachment_path=None) -> str:
             )
             creds = flow.run_local_server(
                 port=GOOGLE_REDIRECT_URL_PORT,
-                host=GOOGLE_JAVASCRIPT_ORIGIN
+                host=GOOGLE_JAVASCRIPT_ORIGIN,
                 redirect_uri_trailing_slash=False,
                 # open_browser=True,
                 authorization_prompt_message="Please visit this URL: {GOOGLE_JAVASCRIPT_ORIGIN}",
